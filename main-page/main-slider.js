19a7102b93c4d078c2f9b940bfcdfd0c6a1c77cc
Mojtaba Adineh <codewithmoji@gmail.com>
2023-03-18 12:07:48 +0330

    damdari&ashayer 3page website no logic

---
+++ main-page/main-slider.js
@@ -0,0 +1,31 @@
+// let slideIndex = 1;
+// showSlides(slideIndex);
+
+// function plusSlides(index) {
+//     showSlides(slideIndex += 1)
+// }
+
+// function currentSlide(index) {
+//     showSlides(slideIndex = index);
+// }
+
+// function showSlides(index) {
+//   let slides = document.querySelector(".mySlides");
+//   let dots = document.querySelector(".navigation-dots");
+
+//   if (index > slides.length) {slideIndex = 1};
+//   if (index < 1) {slideIndex = slides.length};
+
+//   for (let i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//   }
+//   for (let i = 0; i < dots.length; i++) {
+//     dots[i].className = dots[i].className.replace("active" , "");
+//   }
+
+//   slides[slideIndex - 1].style.display = "block";
+//   dots[slideIndex - 1].className += "active";
+// }
+
+
+
